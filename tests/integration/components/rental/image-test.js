be8a5b882c59520d64af15c32de472451e6fb34b
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-25 15:44:34 +0300

    Added component test for Rental::Image

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -6,21 +6,16 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | rental/image', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<Rental::Image />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
+  test('it renders the given image', async function(assert) {
     await render(hbs`
-      <Rental::Image>
-        template block text
-      </Rental::Image>
+      <Rental::Image
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.dom('.image').exists();
+    assert.dom('.image img').hasAttribute('src', '/assets/images/teaching-tomster.png');
+    assert.dom('.image img').hasAttribute('alt', 'Teaching Tomster');
   });
 });
