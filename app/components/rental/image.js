77106687df5476e7c202a283fa4660c73e97520a
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-25 16:08:41 +0300

    Toggle image size

--- app/components/rental/image.js
+++ app/components/rental/image.js
@@ -1,8 +1,11 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class RentalImageComponent extends Component {
-  constructor (...args) {
-    super(...args);
-    this.isLarge = false;
+  @tracked isLarge = false;
+
+  @action toggleSize() {
+    this.isLarge = !this.isLarge;
   }
 }
