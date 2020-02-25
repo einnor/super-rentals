dd15e84836c7f1b9d784127d6f6499e22083d2b3
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-25 13:42:45 +0300

    Added acceptance test for navigation using the nav-bar

--- tests/acceptance/super-rentals-test.js
+++ tests/acceptance/super-rentals-test.js
@@ -46,4 +46,22 @@ module('Acceptance | super rentals', function(hooks) {
 
     assert.equal(currentURL(), '/about');
   });
+
+  test('navigating using the nav-bar', async function(assert) {
+    await visit('/');
+
+    assert.dom('nav').exists();
+    assert.dom('nav a.menu-index').hasText('SuperRentals')
+    assert.dom('nav a.menu-about').hasText('About');
+    assert.dom('nav a.menu-contact').hasText('Contact');
+
+    await click('nav a.menu-about');
+    assert.equal(currentURL(), '/about');
+
+    await click('nav a.menu-contact');
+    assert.equal(currentURL(), '/getting-in-touch');
+
+    await click('nav a.menu-index');
+    assert.equal(currentURL(), '/');
+  });
 });
