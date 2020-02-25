5a5a200b031e63d8f4348b1989d0cd59428674ae
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-25 18:40:31 +0300

    Map, transform and render

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,27 @@
 import Route from '@ember/routing/route';
 
+const COMMUNITY_CATEGORIES = [
+  'Condo',
+  'Townhouse',
+  'Apartment'
+];
+
 export default class IndexRoute extends Route {
   async model() {
     const response = await fetch('/api/rentals.json');
-    const parsed = await response.json();
-    return parsed;
+    const { data } = await response.json();
+
+    return data.map(model => {
+      const { attributes } = model;
+      let type;
+
+      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
+        type = 'Community';
+      } else {
+        type = 'Standalone';
+      }
+
+      return { type, ...attributes };
+    });
   }
 }
