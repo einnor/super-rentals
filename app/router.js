78ebeabc12da120f46b3a0124079528463c4fcaa
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-26 14:27:25 +0300

    Test the dynamic route

--- app/router.js
+++ app/router.js
@@ -9,5 +9,5 @@ export default class Router extends EmberRouter {
 Router.map(function() {
   this.route('about');
   this.route('contact', { path: '/getting-in-touch' });
-  this.route('rental', { path: '/rental/:rental_id'});
+  this.route('rental', { path: '/rentals/:rental_id'});
 });
