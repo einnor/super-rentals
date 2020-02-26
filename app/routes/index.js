089740c7ffbd5adcf31702bf9cad718ce47e0b9d
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-26 16:56:35 +0300

    Refactoring

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,27 +1,9 @@
 import Route from '@ember/routing/route';
-
-const COMMUNITY_CATEGORIES = [
-  'Condo',
-  'Townhouse',
-  'Apartment'
-];
+import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
+  @service store;
   async model() {
-    const response = await fetch('/api/rentals.json');
-    const { data } = await response.json();
-
-    return data.map(model => {
-      const { id, attributes } = model;
-      let type;
-
-      if (COMMUNITY_CATEGORIES.includes(attributes.category)) {
-        type = 'Community';
-      } else {
-        type = 'Standalone';
-      }
-
-      return { id, type, ...attributes };
-    });
+    return this.store.findAll('rental');
   }
 }
